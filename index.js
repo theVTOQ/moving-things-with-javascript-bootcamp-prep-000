e07fc247983bd0ff1a2864ac1a54e5b310187c3c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-23 00:01:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much."
+
+var dodger = docment.getElementById('dodger');
+
+dodger.addEventListener('keydown', function(e){
+  if(e.which === 39){
+    moveDodgerRight();
+  }
+})
+
+function moveDodgerLeft(){
+  var currentLeft = parseInt(dodger.style.left.replace('px', ''));
+  dodger.left = `${currentLeft - 1}px`;
+}
+
+function moveDodgerRight(){
+  var currentRight = parseInt(dodger.style.right.replace('px', ''));
+  dodger.right = `${currentRight - 1}px`;
+}
